e52d9bf8cb3afc77c6fadcedec5b12c1bd40f5b1
César Gonzalez <cedgo1997@gmail.com>
2020-10-27 17:03:50 -0400

    Navbar component ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 //Modules
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, UsersModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, UsersModule],
   providers: [],
   bootstrap: [AppComponent],
 })
