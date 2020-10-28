63b6561646cbf97ad897af25aa13729b3b911feb
César Gonzalez <cedgo1997@gmail.com>
2020-10-27 21:57:28 -0400

    get request from api in list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 //Modules
@@ -10,7 +10,13 @@ import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, UsersModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsersModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: [],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(public usersService: UserService) {}
 
   ngOnInit(): void {
+    this.usersService.getUsers().subscribe((users) => console.log(users));
   }
 
+  /*   getUsers() {
+    this.usersService.getUsers()
+    .subscribe((users) => console.log(users));
+  } */
 }
