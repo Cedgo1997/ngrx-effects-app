5cf8f55479b83c47cfe481586492ca23c78c8a4e
César Gonzalez <cedgo1997@gmail.com>
2020-10-29 13:53:41 -0400

    user reducer ready

---
+++ src/app/store/reducers/user.reducer.ts
@@ -0,0 +1,46 @@
+import { createReducer, on } from '@ngrx/store';
+import { loadUser, loadUserError, loadUserSuccess } from '../actions';
+import { UserModel } from './../../models/user.model';
+
+export interface UserState {
+  id: string;
+  user: UserModel;
+  loaded: boolean;
+  loading: boolean;
+  error: any;
+}
+
+export const userInitialState: UserState = {
+  id: null,
+  user: null,
+  loaded: false,
+  loading: false,
+  error: null,
+};
+
+const _userReducer = createReducer(
+  userInitialState,
+
+  on(loadUser, (state, { id }) => ({ ...state, loading: true, id })),
+
+  on(loadUserSuccess, (state, { user }) => ({
+    ...state,
+    loading: false,
+    loaded: true,
+    user: { ...user },
+  })),
+  on(loadUserError, (state, { payload }) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    error: {
+      url: payload.url,
+      name: payload.name,
+      message: payload.message,
+    },
+  }))
+);
+
+export function userReducer(state, action) {
+  return _userReducer(state, action);
+}
